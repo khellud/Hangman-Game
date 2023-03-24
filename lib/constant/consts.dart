078c0ac09278e6d6992bff80426003374922d1c1
khellud <khalidhamid993@gmail.com>
2023-03-24 13:46:40 +0300

    first commit

---
+++ lib/constant/consts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backGroundColor = Color(0xff2c3333);
+  static const textColor = const Color(0xffE7F6F2);
+}
+
+class GameUI {
+  static const body = "assets/images/body.png";
+  static const hang = "assets/images/hang.png";
+  static const head = "assets/images/head.png";
+  static const leftArm = "assets/images/l_arm.png";
+  static const rightArm = "assets/images/r_arm.png";
+  static const leftLeg = "assets/images/l_leg.png";
+  static const rightLeg = "assets/images/r_leg.png";
+}
---
+++ lib/game_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+      ),
+      body: Container(),
+    );
+  }
+}
