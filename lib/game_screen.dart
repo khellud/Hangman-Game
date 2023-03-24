879b8a54b21786e679eb64d3b653cee07ba40a25
khellud <khalidhamid993@gmail.com>
2023-03-24 14:40:11 +0300

    ui

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constant/consts.dart';
+
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
 
@@ -8,13 +10,66 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
+  var charac = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Hangman"),
         elevation: 0.0,
+        backgroundColor: Colors.transparent,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.amber,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.count(
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                crossAxisCount: 7,
+                children: charac.split("").map((e) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black38,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      e,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
